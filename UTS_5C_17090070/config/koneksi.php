f9b9bf1220f66403ed974586f61dd14c5c66bde4
widiastuti09 <widiastuti.1299@gmail.com>
2019-11-09 15:32:13 +0700

    uts commit

---
+++ UTS_5C_17090070/config/koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "dbtegalbahari";
+
+// Koneksi dan memilih database di server
+mysql_connect($server,$username,$password) or die(
+    "Koneksi gagal");
+mysql_select_db($database) or die("Database tidak 
+    bisa dibuka");
+?>
+
